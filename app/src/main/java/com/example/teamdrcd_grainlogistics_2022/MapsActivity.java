7698f40a91a42aaff9adf0c0926cb33d0b6840c8
Ms. Rebecca Carton <rcarton@bettendorf.k12.ia.us>
2022-03-08 14:13:18 -0600

    Tuesday's work

--- app/src/main/java/com/example/teamdrcd_grainlogistics_2022/MapsActivity.java
+++ app/src/main/java/com/example/teamdrcd_grainlogistics_2022/MapsActivity.java
@@ -44,8 +44,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng quadcities = new LatLng(42, -90);
+        mMap.addMarker(new MarkerOptions().position(quadcities).title("Marker in Bettendorf"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(quadcities));
     }
 }
