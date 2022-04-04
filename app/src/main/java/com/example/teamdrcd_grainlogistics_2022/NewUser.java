e8fd2e1f0baa64f5456bade42854c8f9fc3af52b
dkatragadda1 <98508075+dkatragadda1@users.noreply.github.com>
2022-04-04 13:56:43 -0500

    Update

--- app/src/main/java/com/example/teamdrcd_grainlogistics_2022/NewUser.java
+++ app/src/main/java/com/example/teamdrcd_grainlogistics_2022/NewUser.java
@@ -3,11 +3,13 @@ package com.example.teamdrcd_grainlogistics_2022;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -15,6 +17,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.*;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Locale;
 
 
 public class NewUser extends AppCompatActivity {
@@ -77,16 +81,36 @@ public class NewUser extends AppCompatActivity {
 
                             firstName = findViewById(R.id.editTextTextPersonName4);
                             String fName = firstName.getText().toString();
+                            fName = fName.toLowerCase();
+                            String finitial = fName.substring(0,1);
+                            fName = finitial.toUpperCase() + fName.substring(1);
                             DatabaseReference myRef = database.getReference("/users/" + uid + "/First Name");
                             myRef.setValue(fName);
 
                             lastName = findViewById(R.id.editTextTextPersonName5);
                             String lName = lastName.getText().toString();
+                            lName = lName.toLowerCase();
+                            String linitial = lName.substring(0,1);
+                            lName = linitial.toUpperCase() + lName.substring(1);
                             DatabaseReference myRef1 = database.getReference("/users/" + uid + "/Last Name");
                             myRef1.setValue(lName);
 
                             phoneNum = findViewById(R.id.editTextTextPersonName7);
-                            Long pn = Long.parseLong(phoneNum.getText().toString());
+                            Long pn = 0L;
+
+                            try {
+                                pn = Long.parseLong(phoneNum.getText().toString());
+                            }
+                            catch (Exception e)
+                            {
+                                String sPhoneNum = phoneNum.getText().toString();
+                                sPhoneNum.replace("-", "");
+                                sPhoneNum.replace("(", "");
+                                sPhoneNum.replace(")", "");
+                                sPhoneNum.replace(" ", "");
+                                pn = Long.parseLong((sPhoneNum));
+                            }
+
                             DatabaseReference myRef2 = database.getReference("/users/" + uid + "/Phone Number");
                             myRef2.setValue(pn);
                         } else {
@@ -94,12 +118,51 @@ public class NewUser extends AppCompatActivity {
                             //Log.w(TAG, "createUserWithEmail:failure", task.getException());
                             //Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
                                     //Toast.LENGTH_SHORT).show();
-                            updateUI(null);
+                            // If sign in fails, display a message to the user.
+                            //Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                            //Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                            String exception = task.getException().toString();
+                            for(int i = 0; i < exception.length(); i++)
+                            {
+                                if(exception.charAt(i) == ':')
+                                {
+                                    exception = exception.substring(i + 2);
+                                }
+                            }
+
+                            String suc = "Login Failed: \n" + exception;
+                            final TextView helloTextView = (TextView) findViewById(R.id.textView10);
+                            helloTextView.setText(suc);
+
                         }
                     }
                 });
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    updateUI(user);
+                                }
+                                else {
+                                    // If sign in fails, display a message to the user.
+                                    //Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    //Toast.LENGTH_SHORT).show();
+                                    //updateUI(null);
+                                    String suc = "Login Failed. Your email or password was incorrect.";
+                                    //final TextView helloTextView = (TextView) findViewById(R.id.textView);
+                                    //helloTextView.setText(suc);
+                                }
+                            }
+                        }
+                );
     }
 
     private void updateUI(FirebaseUser user) {
+        startActivity(new Intent(NewUser.this, MapsActivity.class));
     }
 }
